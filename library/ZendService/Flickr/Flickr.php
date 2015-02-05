91d98c44a9cc1701d255c33fd0220a06e54f7217
emontes <emontes@turista.com.mx>
2015-02-05 15:47:54 -0500

    Added New Method getPhotoExif

--- library/ZendService/Flickr/Flickr.php
+++ library/ZendService/Flickr/Flickr.php
@@ -272,6 +272,52 @@ class Flickr
     }
     
     /**
+     * Get the Photo Exifs as an array
+     *
+     * @param  string $id id of the photo
+     * @return array
+     */
+    public function getPhotoExif($id)
+    {
+        static $method = 'flickr.photos.getExif';
+    
+        $options = array('api_key' => $this->apiKey, 'method' => $method, 'photo_id' => (string)$id);
+    
+        if (empty($id)) {
+            throw new Exception\InvalidArgumentException('You must supply a Photo id');
+        }
+    
+        $request = new HttpRequest;
+        $request->setUri(self::URI_BASE);
+        $request->getQuery()->fromArray($options);
+        $response = $this->httpClient->send($request);
+    
+        if ($response->isServerError() || $response->isClientError()) {
+            throw new Exception\RuntimeException('An error occurred sending request. Status code: '
+                . $response->getStatusCode());
+        }
+    
+        $dom = new DOMDocument();
+        $dom->loadXML($response->getBody());
+        self::checkErrors($dom);
+        $xpath = new DOMXPath($dom);
+        $photo = $xpath->query('//photo')->item(0);
+        $retval = array();
+        foreach ($photo->childNodes as $item) {
+            if ($item->nodeName <> '#text'){
+                $label = $item->getAttribute('label');
+                foreach ($item->childNodes as $item2) {
+                    if ($item2->nodeName <> '#text'){
+                        $retval[$label] = $item2->nodeValue;
+                    }
+                }
+            }
+    
+        }
+        return $retval;
+    }
+    
+    /**
      * Get the Photo Info as an array
      *
      * @param  string $id id of the photo
